a9f497f10b3b85c8e340a14f2e77fe04aea8b984
MarJesSolP <usr3597@salesianos-lacuesta.net>
2023-05-15 20:03:33 +0100

    feat: creado el menÃu, además de método adicionales para la correcta interacción entre menÃu y diccionario

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,75 @@
+import com.duolingo.entities.Diccionario;
+import com.duolingo.utilities.depurador;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-    }
+
+        int option = 7;
+        String word, initial;
+        Scanner keyboard = new Scanner(System.in);
+        Diccionario dictionary = new Diccionario();
 
 
+
+            while (option != 6) {
+
+                System.out.println("Welcome to your dictionary");
+                System.out.println("Please use one of the following options");
+                System.out.println("1. Add a word");
+                System.out.println("2. Delete a word");
+                System.out.println("3. Would you like to know if a word already exists?");
+                System.out.println("4. Display your existing initials list");
+                System.out.println("5. Display you word lists");
+                System.out.println("6. Close the dictionary");
+
+                option = keyboard.nextInt();
+                keyboard.nextLine();
+
+                switch (option) {
+
+                    case 1:
+                        System.out.println("Please write the word");
+                        word = keyboard.nextLine();
+                        word = depurador.depuradorPalabritas(word);
+                        dictionary.agregarPalabrita(word);
+                        System.out.println("Word successfully added");
+                        break;
+                    case 2:
+                        System.out.println("Which word do you want to delete?");
+                        word = keyboard.nextLine();
+                        word = depurador.depuradorPalabritas(word);
+                        dictionary.eliminarPalabrita(word);
+                        break;
+                    case 3:
+                        System.out.println("Enter a word");
+                        word = keyboard.nextLine();
+                        word = depurador.depuradorPalabritas(word);
+                        dictionary.palabrasIguales(word);
+                        break;
+                    case 4:
+                        dictionary.mostrarIniciales();
+                        break;
+                    case 5:
+                        System.out.println("Please enter an initial");
+                        initial = keyboard.nextLine();
+                        initial = depurador.depuradorPalabritas(initial);
+                        dictionary.mostrarPalabras(initial);
+                        break;
+                    case 6:
+                        System.out.println("Closing the dictionary. Bye!...");
+                        break;
+                    default:
+                        System.out.println("Please enter a number from 1 to 6");
+
+                }
+
+
+            }
+
+
+        }
 
 }
--- src/com/duolingo/entities/Diccionario.java
+++ src/com/duolingo/entities/Diccionario.java
@@ -6,22 +6,63 @@ import java.util.Map;
 import java.util.Set;
 
 public class Diccionario {
-    private Map<String,Set<String>> palabritas = new HashMap<>();
+    private Map<String,Set<String>> diccionario = new HashMap<>();
 
     public void agregarPalabrita(String palabrita){
         String inicialPalabrita;
         inicialPalabrita = palabrita.substring(0,1);
 
-        if(palabritas.containsKey(inicialPalabrita)){
-            palabritas.get(inicialPalabrita).add(palabrita);
+        if(diccionario.containsKey(inicialPalabrita)){
+            diccionario.get(inicialPalabrita).add(palabrita);
 
         }else{
             Set<String> nuevaColeccion = new HashSet<>();
-            palabritas.put(inicialPalabrita,nuevaColeccion);
-            palabritas.get(inicialPalabrita).add(palabrita);
+            diccionario.put(inicialPalabrita,nuevaColeccion);
+            diccionario.get(inicialPalabrita).add(palabrita);
+        }
+    }
+    public void eliminarPalabrita(String palabraEliminar){
+        boolean palabraEliminada= false;
+        for(Set<String> palabras : diccionario.values()){
+            if(palabras.contains(palabraEliminar)){
+                palabras.remove(palabraEliminar);{
+                    palabraEliminada = true;
+                    break;
+                }
+            }
         }
 
-
+        if(palabraEliminada){
+           System.out.println("Word successfully delete");
+            System.out.println("************************************");
+        }else {
+            System.out.println("The word doesn't exist in the dictionary");
+            System.out.println("************************************");
+        }
+    }
+    public void palabrasIguales(String palabraComparar){
+        boolean existePalabrita = false;
+        for(Set<String> palabras : diccionario.values()){
+            if(palabras.contains(palabraComparar)){
+                existePalabrita = true;
+                break;
+            }
+        }
+        if(existePalabrita){
+            System.out.println("The word already exist");
+            System.out.println("************************************");
+        }else {
+            System.out.println("the word does not exist in the dictionary");
+            System.out.println("************************************");
+        }
+    }
+    public void mostrarIniciales (){
+        System.out.println( "List of available initials " + diccionario.keySet());
+        System.out.println("************************************");
     }
 
+    public void mostrarPalabras(String inicialSolicitada){
+        System.out.println("List of available words" + diccionario.get(inicialSolicitada));
+        System.out.println("************************************");
+    }
 }
