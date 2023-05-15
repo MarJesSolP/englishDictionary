9cbe6a345ed48b91a4097c76140e903e8457b7e4
MarJesSolP <usr3597@salesianos-lacuesta.net>
2023-05-15 13:04:49 +0100

    feat:Creada la clase depurador para normalizar las palabras introducidas.

---
+++ src/com/duolingo/utilities/depurador.java
@@ -0,0 +1,12 @@
+package com.duolingo.utilities;
+
+public class depurador {
+    public static String depuradorPalabritas (String palabruska){
+        String palabraLimpita;
+        palabraLimpita = palabruska.toLowerCase();
+        palabraLimpita = palabraLimpita.trim();
+        return palabraLimpita;
+
+    }
+}
+
